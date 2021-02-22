8a45d3c908ac92e8d7f99ac5eafd654679869e0f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-02-22 23:17:29 +0900

    fix: example code

---
+++ example/main_test.go
@@ -0,0 +1,31 @@
+package main_test
+
+import (
+	"testing"
+
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	user = "linuxserver.io"
+	host = "127.0.0.1"
+	port = "2222"
+	pass = "password"
+)
+
+func TestConnection(t *testing.T) {
+	cfg := &ssh.ClientConfig{
+		User:            user,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Auth: []ssh.AuthMethod{
+			ssh.Password(pass),
+		},
+	}
+	conn, err := ssh.Dial("tcp", host+":"+port, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	t.Logf("Successful connection to ssh server.  password: %s\n", pass)
+}
